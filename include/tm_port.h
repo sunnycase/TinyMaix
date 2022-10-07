dbcb10abca7c14d9eeb93d9946474d49f26f12f5
sunnycase <sunnycase@live.cn>
2022-10-07 18:09:47 +0800

    Port to w801

--- include/tm_port.h
+++ include/tm_port.h
@@ -13,6 +13,8 @@ limitations under the License.
 #ifndef __TM_PORT_H
 #define __TM_PORT_H
 
+#include "wm_include.h"
+
 #define TM_ARCH_CPU         (0) //default, pure cpu compute
 #define TM_ARCH_ARM_SIMD    (1) //ARM Cortex M4/M7, etc.
 #define TM_ARCH_ARM_NEON    (2) //ARM Cortex A7, etc.
@@ -26,8 +28,8 @@ limitations under the License.
 
 /******************************* PORT CONFIG  ************************************/
 #define TM_ARCH         TM_ARCH_CPU
-#define TM_OPT_LEVEL    TM_OPT0
-#define TM_MDL_TYPE     TM_MDL_INT8
+#define TM_OPT_LEVEL    TM_OPT1
+#define TM_MDL_TYPE     TM_MDL_FP32
 #define TM_FASTSCALE    (0)         //enable if your chip don't have FPU, may speed up 1/3, but decrease accuracy
 #define TM_LOCAL_MATH   (0)         //use local math func (like exp()) to avoid libm
 #define TM_ENABLE_STAT  (1)         //enable mdl stat functions
@@ -49,9 +51,9 @@ limitations under the License.
 /******************************* DBG TIME CONFIG  ************************************/
 #include <sys/time.h>
 #include <time.h>
-#define  TM_GET_US()       ((uint32_t)(clock()*1000000/CLOCKS_PER_SEC))
+#define  TM_GET_US()       ((float)tls_os_get_time()*1000000/HZ)
 
-#define TM_DBGT_INIT()     uint32_t _start,_finish;float _time;_start=TM_GET_US();
+#define TM_DBGT_INIT()     float _start,_finish;float _time;_start=TM_GET_US();
 #define TM_DBGT_START()    _start=TM_GET_US();
 #define TM_DBGT(x)         {_finish=TM_GET_US();\
                             _time = (float)(_finish-_start)/1000.0;\
